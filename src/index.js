5788a0f3b2dd0ae66adc05b94fa552ffc3235657
BIC-JSS5794\User <jss5794@penta.co.kr>
2023-12-13 17:54:40 +0900

    리액트2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client'; // ReactDOM 은 React18 최신 가져오기에서 지원안함!!!
 import {createRoot} from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import Book from "./chapter_03/Book"; // 빈 껍데기일 뿐!
-// import Library from "./chapter_03/Library"; // 실제 내용을 import 해와야함!
-// import Clock from "./chapter_04/Clock";
-// import CommentList from "./chapter_05/CommentList";
-// import NotificationList from "./chapter_06/NotificationList";
-// import Accommodate from "./chapter_07/Accommodate";
-// import Example from "./chapter_07/Example";
-// import ConfirmButton from "./chapter_08/ConfirmButton";
+import Book from "./chapter_03/Book"; // 빈 껍데기일 뿐!
+import Library from "./chapter_03/Library"; // 실제 내용을 import 해와야함!
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
+import Accommodate from "./chapter_07/Accommodate";
+import Example from "./chapter_07/Example";
+import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 
