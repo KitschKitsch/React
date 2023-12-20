c67c9862747d5da1970fd49f1c7a89afbc89caeb
BIC-JSS5794\User <jss5794@penta.co.kr>
2023-12-20 09:22:54 +0900

    처음 만나는 리액트

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import Blocks from "./chapter_15/Blocks";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
         /*<React.StrictMode> {/!*18 이후로는 개발모드에서 컴포넌트 unmount->remount 시킴(버그찾으려고)*!/}*/
-            < Blocks />
+            < ProfileCard />
         /*</React.StrictMode>,*/
 );
 
